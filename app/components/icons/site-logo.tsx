fd6408e6fa0612b4170e3fa81170087cfb1e05ec
Toshiki Okazaki <toshiki.okazaki@gree.net>
2025-06-28 21:07:09 +0900

    feat: update site-logo & site-header

--- app/components/icons/site-logo.tsx
+++ app/components/icons/site-logo.tsx
@@ -7,12 +7,13 @@ export const SiteLogo = (props: IconProps) => {
       viewBox="0 0 256 256"
       fill="none"
       stroke="currentColor"
-      strokeWidth="24"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+      stroke-width="16"
+      stroke-linecap="round"
+      stroke-linejoin="round"
       {...props}
     >
-      <path d="M64 48V208M192 48V208M64 128H192" />
+      <path d="M128 240c-40-24-64-56-64-96 0-36 22.5-66 56-88 0 24 8 40 24 56s24 32 24 56c0 24-8 48-40 72z" />
+      <path d="M128 208c-20-12-32-28-32-48 0-18 11-34 28-46 0 12 4 20 12 28s12 16 12 28c0 12-4 24-20 38z" />
     </svg>
   );
 };
